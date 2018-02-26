b891fae752bca2e549acae870520b0d285d42318
vagrant <vagrant@vagrant.vm>
2018-02-26 23:15:58 +0000

    Staging edit_profile.js

---
+++ static/edit_profile.js
@@ -0,0 +1,42 @@
+// Edit bio
+
+function confirmBlurb() {
+	$('#blurb-form').empty();
+	$('#blurb-notif').html('Your bio has been updated.');
+	$('#blurb-notif').fadeIn(200).fadeOut(200).fadeIn(200);
+}
+
+function confirmEdit() {
+	$('#edit-blurb-form').empty();
+	$('#blurb-notif').html('Your bio has been updated.');
+	$('#edit-blurb-notif').fadeIn(200).fadeOut(200).fadeIn(200);
+}
+
+function submitNewBlurb(evt) {
+	evt.preventDefault();
+	evt.stopImmediatePropagation();
+
+	let blurbData = $('#blurb-form').serialize();  // IMPORTANT FOR SERVER SIDE
+
+	$.post('/new-blurb.json',
+		   blurbData,  // IMPORTANT
+		   confirmBlurb);	
+
+	return false;
+}
+
+function editBlurb(evt) {
+	evt.preventDefault();
+	evt.stopImmediatePropagation();
+
+	let blurbData = $('#update-blurb-form').serialize();
+
+	$.post('/edit-blurb.json',
+			blurbData,
+			confirmEdit);
+
+	return false;
+}
+
+$('#submit-blurb').on('click', submitNewBlurb);
+$('#change-blurb').on('click', editBlurb);
