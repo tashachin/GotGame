2b91be4bf7552a16ea18d1eb8f140a709829b79c
vagrant <vagrant@vagrant.vm>
2018-03-02 01:05:53 +0000

    Second Selenium test works.

--- s_tests.py
+++ s_tests.py
@@ -2,6 +2,8 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 ###################################################
 # SELENIUM TESTS
@@ -11,18 +13,33 @@ class BrowserTests(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 10)  # Used for following redirects
 
 
     def tearDown(self):
-        print "Testing"
         self.browser.quit()
-
 
 
     def test_title(self):
         self.browser.get('http://localhost:5000/')
         self.assertEqual(self.browser.title, 'Got Game?')
 
+
+    def test_login(self):
+        self.browser.get('http://localhost:5000/login')
+
+        username = self.browser.find_element_by_id('user')
+        username.send_keys('markiplier')
+
+        password = self.browser.find_element_by_id('password')
+        password.send_keys('markiplier')
+
+        btn = self.browser.find_element_by_id('submit')
+        btn.click()
+
+        self.wait.until(EC.url_to_be('http://localhost:5000/'))
+        self.assertIn('Logged in.', self.browser.page_source)
+        
 
 if __name__ == "__main__":
     unittest.main()
